3cec2e476e5a06b79088e4c56e45250280b600e8
gabbe129 <Gabriel.israelsson@live.se>
2019-12-19 15:53:13 +0100

    commit

--- src/BookingTreatment.js
+++ src/BookingTreatment.js
@@ -9,7 +9,7 @@ class BookingTreatment extends Component {
     render() {
         return (
        <div>
-           hallå
+           hallå ELller
            <DropdownButton id="dropdown-basic-button" title="Dropdown button">
   <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
   <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
