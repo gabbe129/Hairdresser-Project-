a37a687eea1d94a3af64752717e89d9229fbe7f1
Pontushansson7 <47177677+Pontushansson7@users.noreply.github.com>
2019-12-20 11:05:57 +0100

    treatment

--- src/BookingTreatment.js
+++ src/BookingTreatment.js
@@ -9,7 +9,6 @@ class BookingTreatment extends Component {
     render() {
         return (
        <div>
-           hallå ELller tja faslfhf
             <DropdownButton id="dropdown-basic-button" title="Dropdown button">
             <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
             <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
